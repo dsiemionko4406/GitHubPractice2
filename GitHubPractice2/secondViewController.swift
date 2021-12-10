4879be71b214c79f887d15ab21c115a6b878c4f7
David Siemionko <dsiemionko4406@stu.d214.org>
2021-12-10 10:22:04 -0600

    View

---
+++ GitHubPractice2/secondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  secondViewController.swift
+//  GitHubPractice2
+//
+//  Created by David Siemionko on 12/10/21.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
