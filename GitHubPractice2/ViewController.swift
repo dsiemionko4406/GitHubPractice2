e848d51d6095308aeccb007a4a46fc4e4023a059
anowak-hub <anowak4320@stu.d214.org>
2021-12-10 10:35:41 -0600

    Table view code completed.

--- GitHubPractice2/ViewController.swift
+++ GitHubPractice2/ViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   @IBOutlet weak var tableView: UITableView!
+    var animals: [String] = ["Cow", "Pig", "Horse"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+    
 }
 
